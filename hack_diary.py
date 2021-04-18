1100e26c07b127f00fcbda004322f883d21d0509
wsluser1 <wsluser1@DESKTOP-D3T5842>
2021-04-18 21:26:20 +0500

    ref

--- hack_diary.py
+++ hack_diary.py
@@ -23,24 +23,26 @@ def remove_chastisements(schoolkid_name):
 
 def create_commendation(schoolkid_name, subject_title, teacher_name, date):
     schoolkid = Schoolkid.objects.filter(full_name__contains=schoolkid_name)
-    subject = Subject.objects.filter(title__contains=subject_title, year_of_study=schoolkid.year_of_study)
-    teacher = Teacher.objects.filter(full_name__contains=teacher_name)
-    lesson = Lesson.objects.filter(date=date, subject=subject, year_of_study=schoolkid.year_of_study, group_letter=schoolkid.group_letter)
+    if len(schoolkid) == 1:
+        subject = Subject.objects.filter(title__contains=subject_title, year_of_study=schoolkid[0].year_of_study)
+        teacher = Teacher.objects.filter(full_name__contains=teacher_name)
+        if len(subject) == 1:
+            lesson = Lesson.objects.filter(date=date, subject=subject[0], year_of_study=schoolkid[0].year_of_study, group_letter=schoolkid[0].group_letter)
     if len(schoolkid) == 1 and len(subject) == 1 and len(teacher) == 1 and len(lesson) == 1:
-        Commendation.objects.create(text='Хвалю!', created=date, schoolkid=schoolkid, subject=subject, teacher=teacher)
-        print(f"Добавлена похвала ученику {schoolkid.full_name} по предмету {subject.title}, дата: {date}, учитель {teacher.full_name}")
+        print(Commendation.objects.create(text='Хвалю!', created=date, schoolkid=schoolkid[0], subject=subject[0], teacher=teacher[0]))
+        # print(f"Добавлена похвала ученику {schoolkid[0].full_name} по предмету {subject[0].title}, дата: {date}, учитель {teacher[0].full_name}", comm)
     elif len(schoolkid) == 0:
         print(f'Ученика с именем {schoolkid_name} нет в базе данных.')
     elif len(subject) == 0:
-        print(f'Предмета {subject_name} нет в базе данных.')
+        print(f'Предмета {subject_title} нет в базе данных или у данного ученика.')
     elif len(teacher) == 0:
         print(f'Учителя с именем {teacher_name} нет в базе данных.')
     elif len(lesson) == 0:
-        print(f"По предмету {subject_name} на дату: {date} у ученика {schoolkid_name} не было занятий")
+        print(f"По предмету {subject_title} на дату: {date} у ученика {schoolkid_name} не было занятий")
     elif len(schoolkid) > 1:
         print(f'Найдено {len(schoolkid)} учеников с именем {schoolkid_name}:', *schoolkid, sep='\n')
     elif len(subject) > 1:
-        print(f'Найдено {len(subject)} предметов с названием {subject_name}:', *subject, sep='\n')
+        print(f'Найдено {len(subject)} предметов с названием {subject_title}:', *subject, sep='\n')
     elif len(teacher) > 1:
         print(f'Найдено {len(teacher)} учителей с именем {teacher_name}:', *teacher_name, sep='\n')
 
